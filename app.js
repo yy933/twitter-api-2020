14196c3b3c30c1d013d7cf0577f658f324b71118
yy933 <emilyyu025@gmail.com>
2023-04-26 00:53:18 +0800

    modify db connection config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const path = require('path')
 const express = require('express')
 const helpers = require('./_helpers')
